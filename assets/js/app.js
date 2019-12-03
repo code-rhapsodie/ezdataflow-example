1cffac7602f2b90d68d84415a440ef6883e738ca
jbcr <51637606+jbcr@users.noreply.github.com>
2019-12-03 12:32:04 +0100

    add design

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+const jQuery = require('jquery');
+
+require('../css/app.scss');
+require('bootstrap');
+
+require('startbootstrap-freelancer/scss/freelancer.scss');
+require('@fortawesome/fontawesome-free/css/all.css');
---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace AppBundle\Controller;
+
+
+use eZ\Publish\API\Repository\ContentService;
+use eZ\Publish\API\Repository\SearchService;
+use eZ\Publish\API\Repository\Values\Content\LocationQuery;
+use eZ\Publish\API\Repository\Values\Content\Query;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion\LogicalAnd;
+use eZ\Publish\Core\MVC\Symfony\View\ContentView;
+use eZ\Publish\Core\MVC\Symfony\View\View;
+
+
+
+class HomeController
+{
+    public function __invoke(View $view, SearchService $searchService, ContentService $contentService)
+    {
+        /** @var $view ContentView */
+        $query = new LocationQuery([
+            'filter' => new LogicalAnd([
+                new Query\Criterion\ParentLocationId($view->getContent()->contentInfo->mainLocationId),
+                new Query\Criterion\Visibility(Query\Criterion\Visibility::VISIBLE),
+            ]),
+            'sortClauses' => [new Query\SortClause\Location\Priority(Query::SORT_ASC)]
+        ]);
+        $childLocation = $searchService->findLocations($query);
+        $chidrens = [];
+        foreach ($childLocation->searchHits as $searchHit) {
+            $chidrens[] = $contentService->loadContentByContentInfo($searchHit->valueObject->contentInfo);
+        }
+        array_filter($chidrens);
+        $view->addParameters(['children'=> $chidrens]);
+
+        return $view;
+    }
+}
